8000f8b5405809cb1c24b09c2ea8275f2321e6bc
Sender Eisenbach <sender737@gmail.com>
2023-11-27 15:25:35 +0200

    "*"

--- server/index.js
+++ server/index.js
@@ -44,7 +44,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin:[ "http://localhost:4173" , "http://localhost:5173"],
+    origin:"*",
     methods: ["GET", "POST"],
   },
 });
